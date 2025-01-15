2ef21adc19262870d86d5f4cf5453021979be17f
benpoz <benpoznanski@gmail.com>
2025-01-15 17:05:03 +0200

    Made disktest pretty :)

--- disktest.asm
+++ disktest.asm
@@ -1,27 +1,24 @@
-# Move contents of first 8 sectors one sector forward
-
-# Initialize disk command registers
-add $a0, $zero, $zero, $imm1, 7 , 0                     # Set $a0=7                 000
+add $a0, $zero, $zero, $imm1, 7 , 0                     # Set $a0 = 7                 
 out $zero, $imm1, $zero, $imm2, 16 , 100                # Set buffer address
 
-Loop_Read:
-    blt $zero, $a0, $zero, $imm1, end, 0                 # if $a0<0 end loop         002
+READ:
+    blt $zero, $a0, $zero, $imm1, END, 0                # if $a0 < 0 end loop         
     in $a1, $imm1, $zero, $zero, 17, 0                  # Read status
-    beq $zero, $a1, $imm1, $imm2, 1, Loop_Read          # Wait until not busy
+    beq $zero, $a1, $imm1, $imm2, 1, READ               # Wait until not busy
     out $zero, $imm1, $zero, $a0, 15, 1                 # set sector index as $a0  
     out $zero, $imm1, $zero, $imm2, 14, 1               # set disk to read mode
-    add $t0, $a0, $zero, $imm1, 1, 0                    # Set $t0= $a0 +1
+    add $t0, $a0, $zero, $imm1, 1, 0                    # Set $t0 = $a0 + 1
 
-Loop_write:
-    in $a1, $imm1, $zero, $zero, 17, 0                  # Read status               008
-    beq $zero, $a1, $imm1, $imm2, 1, Loop_write         # Wait until not busy
+WRITE:
+    in $a1, $imm1, $zero, $zero, 17, 0                  # Read status               
+    beq $zero, $a1, $imm1, $imm2, 1, WRITE              # Wait until not busy
     out $zero, $imm1, $zero, $t0, 15, 1                 # set sector index as $a0+1  
     out $zero, $imm1, $zero, $imm2, 14, 2               # set disk to write mode
     sub $a0, $a0, $zero, $imm1, 1, 0                    # a0--
-    beq $zero, $zero, $zero, $imm1, Loop_Read, 0        # return to read_loop
+    beq $zero, $zero, $zero, $imm1, READ, 0             # return to read_loop
 
-end:
-    in $a1, $imm1, $zero, $zero, 17, 0                  # Read status               00E
-    beq $zero, $a1, $imm1, $imm2, 1, end                # Wait until not busy
+END:
+    in $a1, $imm1, $zero, $zero, 17, 0                  # Read status               
+    beq $zero, $a1, $imm1, $imm2, 1, END                # Wait until not busy
     out $zero, $imm1, $zero, $imm2, 14, 0               # set disk to write mode
     halt $zero ,$zero ,$zero ,$zero , 0, 0              # finish
