a656146bc220a764d9019b6e56d175ab32b5bc68
benpoz <benpoznanski@gmail.com>
2025-01-15 14:39:58 +0200

    fixed circle

--- circle.asm
+++ circle.asm
@@ -19,8 +19,7 @@ loop_i:
         sub $a1, $t1, $s1, $zero, 0, 0          # calculate dist_y 
         mac $a1, $a1, $a1, $zero, 0, 0          # calculate dist_y^2
         sub $v0, $s0, $a0, $a1, 0, 0            # calculate r^2 - x^2 - y^2
-        mac $v0, $v0, $v0, $zero, 0, 0          # calculate (r^2 - x^2 - y^2)^2
-        blt $zero, $v0, $a2, $imm2, 0, draw   # if (r^2 - x^2 - y^2)^2 < margin (5?) - draw white pixel
+        bgt $zero, $v0, $zero, $imm2, 0, draw   # if (r^2 - x^2 - y^2) < 0 - draw white pixel
     incr_j:   
         add $t1, $t1, $imm1, $zero, 1, 0          # j++ F
         beq $zero, $zero, $zero, $imm1, loop_j, 0   # loop back
